2357ec90da6e8371e41e5422bab2da2c6e84530b
isabelle3030 <isabellependa@gmail.com>
2025-09-26 18:31:07 +0200

    Feature: UC9 Registratiepagina toegevoegd
    -RegisterView.xaml en RegisterView.xaml.cs toegevoegd.
    -RegisterViewModel met properties en command
    -RegisterView gekoppeld aan RegisterViewModel
    -Navigatie van LoginView naar RegisterView

---
+++ Grocery.App/Views/RegisterView.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace YourApp.Views
+{
+    public partial class RegisterView : ContentPage
+    {
+        public RegisterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
